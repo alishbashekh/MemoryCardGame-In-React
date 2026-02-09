3022d5544714675788a84be694c76fa68968af62
Nabiha-Fatima <nabihaf567@gmail.com>
2026-02-09 16:46:42 +0500

    add logics

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,23 +1,44 @@
 import { Cat, Dog, Grape, Heart } from 'lucide-react'
 import Card from './Card'
 
+const cards = [
+  { 
+    icon : Cat,
+    isFlipped : false,
+    isMatched : false,
+  },
+   {
+    icon : Dog,
+    isFlipped : false,
+    isMatched : false,
+  },
+  {
+    
+    icon : Grape,
+    isFlipped : false,
+    isMatched : false,
+  },
+ {
+    icon : Heart,
+    isFlipped : false,
+    isMatched : false,
+  },
+];
+const paircards= [...cards, ...cards].map((card, index)=>({
+  ...card,
+  id : index + 1,
+}))
 
 const Gameboard = () => {
-  const basecards=[Dog,Cat,Grape,Heart];
-  const paircards= [...basecards, ...basecards]
+ 
+  
 
-  const cards= paircards.map((Icon,index)=>({
-    id:index + 1,
-    value:Icon,
-    isFlipped: false,
-    isMatched: false,
-  }))
-  .sort(()=>
-  Math.random()-0.5) ;
+ 
   return (
     <div className='grid grid-cols-4 gap-4'>
-       {cards.map((card)=>(
-        <Card key={card.id} card={card}/>
+       {paircards.map((card, index)=>(
+      
+        <Card key={id} card={card}/>
        ))}
     </div>
   
@@ -25,3 +46,14 @@ const Gameboard = () => {
 }
 
 export default Gameboard;
+
+
+
+ // const cards= paircards.map((Icon,index)=>({
+  //   id:index + 1,
+  //   value:Icon,
+  //   isFlipped: false,
+  //   isMatched: false,
+  // }))
+  // .sort(()=>
+  // Math.random()-0.5) ;
