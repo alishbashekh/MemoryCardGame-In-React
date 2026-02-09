125dbcc94a6da17f555a8f572514f09b72469d33
Nabiha-Fatima <nabihaf567@gmail.com>
2026-02-09 15:53:53 +0500

    update cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@
 const Card = ({card}) => {
   const Icon = card.value;
   return (
-    <div className="w-28 h-40 bg-gradient-to-tl  to-[#5a189a] from-[#10002b] rounded-lg  flex items-center justify-center shadow-lg transition duration-300  shadow-[#5a189a] cursor-pointer"
+    <div className="w-28 h-40 bg-gradient-to-tl  to-[#5a189a] from-[#10002b] rounded-lg  flex items-center justify-center shadow-lg transition duration-300  shadow-[#5a189a] cursor-pointer transform hover:-translate-y-2"
     >
       <Icon size = {45} className="text-[#e7e8ea]"/>
     </div>
